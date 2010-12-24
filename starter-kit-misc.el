24264adc93e6e510bd82df43348f9ed8a7188a76
Tim Morgan <tim@timmorgan.org>
2010-12-24 00:30:37 -0600

    ido has to go.

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -61,14 +61,14 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
-;; ido-mode is like magic pixie dust!
-(when (> emacs-major-version 21)
-  (ido-mode t)
-  (setq ido-enable-prefix nil
-        ido-enable-flex-matching t
-        ido-create-new-buffer 'always
-        ido-use-filename-at-point 'guess
-        ido-max-prospects 10))
+;;; ido-mode is like magic pixie dust!
+;(when (> emacs-major-version 21)
+  ;(ido-mode t)
+  ;(setq ido-enable-prefix nil
+        ;ido-enable-flex-matching t
+        ;ido-create-new-buffer 'always
+        ;ido-use-filename-at-point 'guess
+        ;ido-max-prospects 10))
 
 (set-default 'indent-tabs-mode nil)
 (set-default 'indicate-empty-lines t)
