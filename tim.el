ae29ac0f0493ea98922d43263e2236c7e5e99f38
Tim Morgan <tim@timmorgan.org>
2010-12-24 19:40:55 -0600

    Don't use vimpulse everywhere.

--- tim.el
+++ tim.el
@@ -4,3 +4,6 @@
 (set-face-attribute 'default nil :font "Monaco-9")
 (if (window-system)
     (set-frame-size (selected-frame) 100 115))
+(setq vimpulse-want-vi-keys-in-apropos nil)
+(setq vimpulse-want-vi-keys-in-buffmenu nil)
+(setq vimpulse-want-vi-keys-in-dired nil)
