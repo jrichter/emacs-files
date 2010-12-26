a7780d2863728593df6da5a8fb0854bb7290050d
jrichter <jrichter@jetfive.com>
2010-12-26 01:14:11 -0600

    font size change

--- justin.el
+++ justin.el
@@ -2,7 +2,7 @@
 (require 'vimpulse)
 (require 'color-theme-ir-black)
 (color-theme-ir-black)
-(set-face-attribute 'default nil :font "Monaco-9")
+(set-face-attribute 'default nil :font "Monaco-10")
 (if (window-system)
     (set-frame-size (selected-frame) 100 115))
 (setq vimpulse-want-vi-keys-in-apropos nil)
